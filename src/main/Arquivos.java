a2cb1b1ed98055bfadcce9e8ada5a5e124935e41
Vitor Vilela <vilelavitor305@gmail.com>
2022-10-02 21:41:57 -0300

    Criação da Classe Arquivos
    Classe Arquivos sendo criada contendo seus atributos e seus metodos get/set

--- src/main/Arquivos.java
+++ src/main/Arquivos.java
@@ -4,10 +4,57 @@
  */
 package main;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
 /**
  *
  * @author vitor
  */
 public class Arquivos {
+    private String nome;
+    private Diretorio pai;
+    private ArrayList<String> conteudo = new ArrayList();
+    private String permissao;
+    private String dataCriacao;
     
+    public Arquivos(Diretorio dir) {
+        this.pai = dir;
+        this.permissao = "-rwxrwxrwx";
+        SimpleDateFormat formato = new SimpleDateFormat("MMM dd yyyy HH:mm");
+        this.dataCriacao = formato.format(new Date());
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Diretorio getPai() {
+        return pai;
+    }
+
+    public void setPai(Diretorio pai) {
+        this.pai = pai;
+    }
+
+    public String getPermissao() {
+        return permissao;
+    }
+
+    public void setPermissao(String permissao) {
+        this.permissao = permissao;
+    }
+
+    public String getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(String dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
 }
